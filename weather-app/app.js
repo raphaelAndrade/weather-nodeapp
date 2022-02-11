0cbd0db743e392fc1bceb251714cc77621e96ae9
raphael <raphaelsantosdesigner@gmail.com>
2022-02-10 21:30:53 -0800

    http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,10 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(()=> {
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=0d006444ab32dca51d2c0d41df7d720a&query=37.8267,-122.4233'
 
-
-setTimeout(()=> {
-    console.log('o second')
-}, 0)
-
-console.log('Stopping')
+request({
+    url: url
+}, (error, response)=> {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
