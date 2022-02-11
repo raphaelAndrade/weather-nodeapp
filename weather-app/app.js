73adce4da91b7e8297c12e8dc0d5ea454da81dbd
raphael <raphaelsantosdesigner@gmail.com>
2022-02-10 22:08:50 -0800

    request configuration

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,9 +2,8 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=0d006444ab32dca51d2c0d41df7d720a&query=37.8267,-122.4233'
 
-request({
-    url: url
-}, (error, response)=> {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response)=> {
+    const data = response.body.current
+    console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees and the feelslike is ${data.feelslike} degrees`)
 })
+
